f671967902442ee1abcc4497e69eaf676ee8083d
omer12619 <110417927+omer12619@users.noreply.github.com>
2024-01-06 17:20:13 +0200

    Roatate the player and add a pole to finsh

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag=="Player")
+        {
+            Debug.Log("Succsues");
+        }
+        
+        
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    [SerializeField] float route;
+    Rigidbody2D rdb;
     // Start is called before the first frame update
     void Start()
     {
-        
+         rdb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rdb.AddTorque(route);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rdb.AddTorque(-route);
+        }
+
+
     }
 }
